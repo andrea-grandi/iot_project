4beec2e5fc6c6edc859c565f8d03489e3d91c9a6
Andrea Grandi <andrea.grandi@tiscali.it>
2025-05-27 19:06:56 +0200

    update README

--- control_unit/microcontrollers/arduino/arduino.ino
+++ control_unit/microcontrollers/arduino/arduino.ino
@@ -1,107 +1,57 @@
-#include <Wire.h>
-#include <TMRpcm.h>
-#include <SD.h>
-#include <SPI.h>
+// gate_esp32.ino
+#include <esp_now.h>
+#include <WiFi.h>
 
-const int flexPin0 = A0;
-const int flexPin1 = A1;
-const int flexPin2 = A2;
-const int microphonePin = A3;
+#define RELAY_PIN 5 // GPIO5 or any pin for relay control
+unsigned long lastSeen = 0;
+bool gateOpen = false;
 
-int value0;
-int value1;
-int value2;
-int microphoneValue;
+typedef struct struct_message {
+  bool carIsNear;
+} struct_message;
 
-const int chipSelect = 10;
+struct_message incomingData;
 
-const int MPU_ADDR = 0x68;
-int16_t gyro_x, gyro_y, gyro_z;
+void onReceive(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr),
+           "%02X:%02X:%02X:%02X:%02X:%02X",
+           info->src_addr[0], info->src_addr[1], info->src_addr[2],
+           info->src_addr[3], info->src_addr[4], info->src_addr[5]);
 
-const int speakerPin = 9;
-TMRpcm tmrpcm;
+  Serial.print("Message received from: ");
+  Serial.println(macStr);
 
-unsigned long startTime = 0;
-unsigned int duration = 2000;
-
-char tmp_str[7];
-
-char *convert_int16_to_str(int16_t i) {
-  sprintf(tmp_str, "%6d", i);
-  return tmp_str;
-}
-
-void playMP3(String command) {
-  String filename = command + ".wav";
-
-  if (SD.exists(filename)) {
-    tmrpcm.play(filename.c_str());
-  }
+  // Cast incomingData if you're expecting a struct
+  // Example:
+  // memcpy(&myStruct, incomingData, sizeof(myStruct));
 }
 
 void setup() {
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW); // gate closed initially
 
-  tmrpcm.speakerPin = speakerPin;
-  tmrpcm.setVolume(5);
-
-  Wire.begin();
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-
-  if (!SD.begin(10)) {
-    return;
-  }
+  esp_now_init();
+  esp_now_register_recv_cb(onReceive);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String receivedString = Serial.readStringUntil('\n');
-    // Riproduci MP3 corrispondente alla stringa ricevuta
-    playMP3(receivedString);
-    // Memorizza il tempo di inizio della riproduzione
-    startTime = millis();
-
-    delay(1000);
+  // If more than 10s passed without car, close gate
+  if (gateOpen && millis() - lastSeen > 10000) {
+    closeGate();
   }
-  
-  if (tmrpcm.isPlaying() && (millis() - startTime >= duration)) {
-    tmrpcm.stopPlayback();
-  }
+}
 
-  Serial.flush();
+void openGate() {
+  Serial.println("Opening gate...");
+  digitalWrite(RELAY_PIN, HIGH); // turn on relay to open
+  gateOpen = true;
+}
 
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 7 * 2, true);
-
-  gyro_x = Wire.read() << 8 | Wire.read();
-  gyro_y = Wire.read() << 8 | Wire.read();
-  gyro_z = Wire.read() << 8 | Wire.read();
-
-  value0 = analogRead(flexPin0);
-  value1 = analogRead(flexPin1);
-  value2 = analogRead(flexPin2);
-
-  microphoneValue = analogRead(microphonePin);
-
-  Serial.print(value0);
-  Serial.print(",");
-  Serial.print(value1);
-  Serial.print(",");
-  Serial.print(value2);
-  Serial.print(",");
-  Serial.print(convert_int16_to_str(gyro_x));
-  Serial.print(",");
-  Serial.print(convert_int16_to_str(gyro_y));
-  Serial.print(",");
-  Serial.print(convert_int16_to_str(gyro_z));
-  Serial.print(",");
-  Serial.print(microphoneValue);
-  Serial.println();
-
-  delay(500);
+void closeGate() {
+  Serial.println("Closing gate...");
+  digitalWrite(RELAY_PIN, LOW); // turn off relay to close
+  gateOpen = false;
 }
